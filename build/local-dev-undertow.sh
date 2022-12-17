efb4b0755217bfccf191be573c390a6903187399
Rakesh Kumar <kumar_rakesh@live.in>
2022-12-17 15:00:08 +0530

    Used Apache-HttpClient profile.

--- build/local-dev-undertow.sh
+++ build/local-dev-undertow.sh
@@ -5,7 +5,7 @@ RESTORE=$(echo -en '\033[0m')
 
 # maven profiles to include support in runtime
 MVN_PROFILES="no-shade,Jackson,JDBC-CP-Hikari,JPA-EclipseLink,MyBatis,MySQL,MariaDB,PostgreSQL,MongoDB,Cayenne,OAuth2,\
-Jetty-HttpClient,\
+Apache-HttpClient,\
 Undertow-Server,\
 Aries-SPIFLY"
 echo "${GREEN}"
