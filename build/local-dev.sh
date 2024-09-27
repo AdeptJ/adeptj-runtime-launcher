aebb414b7838e23630891afc9bf6b1462d6527ab
Rakesh Kumar <kumar_rakesh@live.in>
2024-09-27 14:14:28 +0530

    local-dev.sh updated with ProjectReactor maven profile.

--- build/local-dev.sh
+++ build/local-dev.sh
@@ -4,7 +4,7 @@ GREEN=$(echo -en '\033[00;32m')
 RESTORE=$(echo -en '\033[0m')
 
 # maven profiles to include support in runtime
-CORE_PROFILES='Copy-Lib,HikariCP,EclipseLink,MyBatis,MySQL,MariaDB,PostgreSQL,MongoDB,Cayenne,OAuth2,Aries-SPIFLY,GraphQL,'
+CORE_PROFILES='Copy-Lib,HikariCP,EclipseLink,MyBatis,MySQL,MariaDB,PostgreSQL,MongoDB,Cayenne,OAuth2,Aries-SPIFLY,GraphQL,ProjectReactor,'
 
 case "$1" in
   'jetty')
